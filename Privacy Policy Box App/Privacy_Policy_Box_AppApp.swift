f6e77e6dad09a4e0e454667892cde992f5fa246b
Lev <153025879+lev67j@users.noreply.github.com>
2025-02-26 22:56:33 +0300

    Good

---
+++ Privacy Policy Box App/Privacy_Policy_Box_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Privacy_Policy_Box_AppApp.swift
+//  Privacy Policy Box App
+//
+//  Created by Lev Vlasov on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Privacy_Policy_Box_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
